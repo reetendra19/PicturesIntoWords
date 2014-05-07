62e3f937f7922564ab165b8ac4c456122275e173
Greg Tennant <gjtennant@sbcglobal.net>
2014-05-06 17:53:38 -0700

    some partly-built angular word counter

--- application/views/level_one.php
+++ application/views/level_one.php
@@ -1,18 +1,30 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" ng-app>
 <head>
 	<meta charset="utf-8">
 	<title>AL414 Level 1</title>
 	<link rel="stylesheet" type="text/css" href="/assets/css/style.css">
 	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.3.0-beta.5/angular.min.js"></script>
 	<script type="text/javascript">
 		$(document).ready(function()
 		{
+			// var counter = 15
+
+			// $(document).on('keyup', '#entry', function(e)
+			// {
+			// 	if (e.keyCode == 32)
+			// 	{
+			// 		counter -= 1;
+			// 		$('#count').text(counter);
+			// 	}
+			// })
+
 			$('#preference').hide();
 
 			$('#entry').submit(function()
 			{	
-				console.log($('#typing').val().split(' ').length);
+				// console.log($('#typing').val().split(' ').length);
 				// if string is less than 15 words
 				if ($('#typing').val().split(' ').length > 15)
 				{
@@ -36,10 +48,11 @@
 						// clear radio buttons from #preference:
 						$("input:radio[name=vote]").prop('checked', false);
 
-						if(data.complete)
-						{	
-							// see if I can put a no-vote stop-check here ===========
+						$('#wrds').text('15');
 
+
+						if(data.complete)
+						{
 							// on 10th photo, new Next button:
 							$('#next').replaceWith("<a style='text-decoration:underline;' href='/controllers/review/'>Next</a>");
 						}
@@ -68,6 +81,11 @@
 						$('.user_input').val("");
 					}, 'json')
 					return false;
+					
+					$('#entry').focus(function()
+						{
+							$('#wrds').text("{{15 - wordcount.split(' ').length}}");
+						})
 				} 
 			})
 
@@ -95,8 +113,11 @@
 		<!-- ENTRY form visible on page load, switches to hidden after 'Go' button -->
 		<form id='entry' class='center' action='/controllers/process_ex1/entry' method='post'>
 			<p>Describe what you see in this picture in no more than 15 words, using a complete sentence.</p>
-			<input class='user_input' id='typing' type='text' name='user_input' spellcheck='true'>
+			<input class='user_input' id='typing' type='text' name='user_input' spellcheck='true' ng-model='wordcount'>
 			<input type='submit' value='Go'>
+
+			<p>You have <span id='wrds'>{{15 - wordcount.split(' ').length}}</span> words left</p>
+
 		</form>
 
 		<!-- PREFERENCE div hidden at page load, switches to visible after 'Go' button -->
